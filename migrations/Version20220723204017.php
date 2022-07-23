37485a852055aa743e1f1da1bc67ed35163cb60f
JorgeMate <jorgematemartinez@gmail.com>
2022-07-23 22:47:07 +0200

    preliminary Pais table

---
+++ migrations/Version20220723204017.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220723204017 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE pais (id INT AUTO_INCREMENT NOT NULL, name_common VARCHAR(255) NOT NULL, name_official VARCHAR(255) DEFAULT NULL, tld VARCHAR(3) DEFAULT NULL, capital VARCHAR(255) DEFAULT NULL, region VARCHAR(255) DEFAULT NULL, subregion VARCHAR(255) DEFAULT NULL, area INT DEFAULT NULL, population INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE pais');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
---
+++ src/Entity/Pais.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PaisRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PaisRepository::class)]
+class Pais
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column()]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $name_common = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name_official = null;
+
+    #[ORM\Column(length: 3, nullable: true)]
+    private ?string $tld = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $capital = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $region = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $subregion = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $area = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $population = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNameCommon(): ?string
+    {
+        return $this->name_common;
+    }
+
+    public function setNameCommon(string $name_common): self
+    {
+        $this->name_common = $name_common;
+
+        return $this;
+    }
+
+    public function getNameOfficial(): ?string
+    {
+        return $this->name_official;
+    }
+
+    public function setNameOfficial(?string $name_official): self
+    {
+        $this->name_official = $name_official;
+
+        return $this;
+    }
+
+    public function getTld(): ?string
+    {
+        return $this->tld;
+    }
+
+    public function setTld(?string $tld): self
+    {
+        $this->tld = $tld;
+
+        return $this;
+    }
+
+    public function getCapital(): ?string
+    {
+        return $this->capital;
+    }
+
+    public function setCapital(?string $capital): self
+    {
+        $this->capital = $capital;
+
+        return $this;
+    }
+
+    public function getRegion(): ?string
+    {
+        return $this->region;
+    }
+
+    public function setRegion(?string $region): self
+    {
+        $this->region = $region;
+
+        return $this;
+    }
+
+    public function getSubregion(): ?string
+    {
+        return $this->subregion;
+    }
+
+    public function setSubregion(?string $subregion): self
+    {
+        $this->subregion = $subregion;
+
+        return $this;
+    }
+
+    public function getArea(): ?int
+    {
+        return $this->area;
+    }
+
+    public function setArea(?int $area): self
+    {
+        $this->area = $area;
+
+        return $this;
+    }
+
+    public function getPopulation(): ?int
+    {
+        return $this->population;
+    }
+
+    public function setPopulation(?int $population): self
+    {
+        $this->population = $population;
+
+        return $this;
+    }
+}
